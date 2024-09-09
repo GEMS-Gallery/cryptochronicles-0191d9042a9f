6779461e86016d588d2d0b01da1cd2ff06aa63c2
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-09 19:05:53 -0400

    0191d909-0803-7224-ac74-6524fd2aec96

--- frontend/declarations/backend/backend.did.d.ts
+++ frontend/declarations/backend/backend.did.d.ts
@@ -2,17 +2,42 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface Post {
+export interface Invoice {
   'id' : bigint,
-  'title' : string,
-  'body' : string,
-  'author' : string,
+  'clientName' : string,
+  'colorTheme' : string,
+  'logo' : [] | [string],
+  'clientEmail' : string,
+  'companyEmail' : string,
+  'clientAddress' : string,
   'timestamp' : Time,
+  'companyName' : string,
+  'items' : Array<InvoiceItem>,
+  'companyAddress' : string,
+}
+export interface InvoiceItem {
+  'description' : string,
+  'quantity' : number,
+  'price' : number,
 }
 export type Time = bigint;
 export interface _SERVICE {
-  'createPost' : ActorMethod<[string, string, string], bigint>,
-  'getPosts' : ActorMethod<[], Array<Post>>,
+  'createInvoice' : ActorMethod<
+    [
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      Array<InvoiceItem>,
+      string,
+      [] | [string],
+    ],
+    bigint
+  >,
+  'getAllInvoices' : ActorMethod<[], Array<Invoice>>,
+  'getInvoice' : ActorMethod<[bigint], [] | [Invoice]>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- frontend/declarations/backend/backend.did.js
+++ frontend/declarations/backend/backend.did.js
@@ -1,15 +1,41 @@
 export const idlFactory = ({ IDL }) => {
+  const InvoiceItem = IDL.Record({
+    'description' : IDL.Text,
+    'quantity' : IDL.Float64,
+    'price' : IDL.Float64,
+  });
   const Time = IDL.Int;
-  const Post = IDL.Record({
+  const Invoice = IDL.Record({
     'id' : IDL.Nat,
-    'title' : IDL.Text,
-    'body' : IDL.Text,
-    'author' : IDL.Text,
+    'clientName' : IDL.Text,
+    'colorTheme' : IDL.Text,
+    'logo' : IDL.Opt(IDL.Text),
+    'clientEmail' : IDL.Text,
+    'companyEmail' : IDL.Text,
+    'clientAddress' : IDL.Text,
     'timestamp' : Time,
+    'companyName' : IDL.Text,
+    'items' : IDL.Vec(InvoiceItem),
+    'companyAddress' : IDL.Text,
   });
   return IDL.Service({
-    'createPost' : IDL.Func([IDL.Text, IDL.Text, IDL.Text], [IDL.Nat], []),
-    'getPosts' : IDL.Func([], [IDL.Vec(Post)], ['query']),
+    'createInvoice' : IDL.Func(
+        [
+          IDL.Text,
+          IDL.Text,
+          IDL.Text,
+          IDL.Text,
+          IDL.Text,
+          IDL.Text,
+          IDL.Vec(InvoiceItem),
+          IDL.Text,
+          IDL.Opt(IDL.Text),
+        ],
+        [IDL.Nat],
+        [],
+      ),
+    'getAllInvoices' : IDL.Func([], [IDL.Vec(Invoice)], ['query']),
+    'getInvoice' : IDL.Func([IDL.Nat], [IDL.Opt(Invoice)], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
